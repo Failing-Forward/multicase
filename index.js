161afef064705a3eac1c9488b4fd7b50cf056490
Dmitry Mikhaltsov <45086516+bqio@users.noreply.github.com>
2020-04-17 09:53:52 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const MultiCase = require('./lib/MultiCase')
 
 const mcase = new MultiCase('data.json')
 
-console.log(mcase.generate("Привет, { Dimitri.Nominative }."))
-console.log(mcase.generate("Ты о { Dimitri.Prepositional }?"))
-console.log(mcase.generate("Скажи об этом { Dimitri.Dative }!"))
+console.log(mcase.generate("Привет, { Dimitri.Nominative }.")) // Привет, Дмитрий.
+console.log(mcase.generate("Ты о { Dimitri.Prepositional }?")) // Ты о Дмитрии?
+console.log(mcase.generate("Скажи об этом { Dimitri.Dative }!")) // Скажи об этом Дмитрию!
